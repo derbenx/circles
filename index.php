e0a46f7227dd467ef2562698ec63e7b28c0d6377
derbenx <colin@derben.ca>
2025-08-12 00:03:22 -0400

    Add files via upload
    Added all files

---
+++ index.php
@@ -0,0 +1,9 @@
+<html><head>
+<!-- <meta http-equiv="refresh" content="20;url=." /> -->
+<link rel="stylesheet" href="css.css">
+</head>
+<!-- body -->
+<?php include'load.php'; ?>
+<script src='js.js'></script>
+<div id='btg'><a href='..'>back to games</a></div>
+</html>
